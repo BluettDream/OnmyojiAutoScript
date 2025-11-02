9bc22828a7e8a0ca947deca9dc1a6d8584b7f72c
Azure <bluetian1st@gmail.com>
2025-11-02 21:35:10 +0800

    feat(DemonRetreat): delete custom time and add wait time

--- tasks/DemonRetreat/config.py
+++ tasks/DemonRetreat/config.py
@@ -6,17 +6,11 @@ from pydantic import BaseModel, Field
 
 from tasks.Component.GeneralBattle.config_general_battle import GeneralBattleConfig
 from tasks.Component.SwitchSoul.switch_soul_config import SwitchSoulConfig
-from tasks.Component.config_scheduler import Scheduler
+from tasks.Component.config_scheduler import Scheduler, GuildScheduler
 from tasks.Component.config_base import ConfigBase, Time
 
 
-class DemonRetreatTime(ConfigBase):
-    # 自定义运行时间
-    custom_run_time: Time = Field(default=Time(hour=10, minute=0, second=0))
-
-
 class DemonRetreat(ConfigBase):
-    scheduler: Scheduler = Field(default_factory=Scheduler)
-    demon_retreat_time: DemonRetreatTime = Field(default_factory=DemonRetreatTime)
+    scheduler: GuildScheduler = Field(default_factory=GuildScheduler)
     general_battle: GeneralBattleConfig = Field(default_factory=GeneralBattleConfig)
     switch_soul_config: SwitchSoulConfig = Field(default_factory=SwitchSoulConfig)
